754ec3662a9548f6da7930ee005f7001c8439d6e
Antonio725 <ab7719@g.rit.edu>
2022-11-10 10:27:11 +0100

    Add constants

---
+++ lib/constants/assets.dart
@@ -0,0 +1,9 @@
+///
+/// This is a class where all assets are stored
+/// You can then reference them in code with `MyAssets.someAsset`
+///
+
+class MyAssets {
+  static const apexologyImage = 'assets/images/apexology.png';
+  static const apexologyIcon = 'assets/icons/apexology_icon.svg';
+}
